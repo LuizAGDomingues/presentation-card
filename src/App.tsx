2d0675d35fb38e2ed603b1ef3c4b31371ca6ed8c
Luiz Domingues <luizarthur1234@gmail.com>
2022-05-17 22:05:55 -0300

    Primeira versão

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import { useState } from 'react'
+import { Ficha1 } from "./componentes/Ficha1"
 
 function App() {
   return (
-    <main className='bg-card bg-no-repeat bg-center bg-cover w-screen min-h-screen h-screen text-gray font-medium text-lg font-Roboto grid'>
-      <div className='max-w-sm mx-auto text-center'>
-          <div>
-              <img src="./images/foto.jfif" alt="Foto Pessoal" className="foto"/>
-          </div>
-          <h1 className='text-2xl mt-6 font-bold'>Luiz Domingues</h1>
-          <a href="https://github.com/LuizAGDomingues" target="_blank" className='text-gray mt-1 mx-28 p-0 flex gap-3 justify-center'>
-            <img src="./images/github.svg" alt="ícone github" />    
-          LuizAGDomingues</a>
-          <p className='mt-8 p-1'>Electrical engineer focused on learning programming</p>
-          <ul className='mt-12 list-none p-0 flex gap-2 justify-center'>
-            <li>
-              <a href="https://facebook.com/luiz.domingues.509" target="_blank">
-                <img src="./images/facebook.svg" alt="ícone facebook" className='hover:border-2 border-purple-300'/></a>
-            </li>
-            <li>
-              <a href="https://twitter.com/Luiz_gDomingues" target="_blank">
-                <img src="./images/twitter.svg" alt="icone twitter" className='hover:border-2 border-purple-300'/></a>
-            </li>
-            <li>
-              <a href="https://instagram.com/luiz_gdomingues/" target="_blank">
-                <img src="./images/instagram.svg" alt="icone instagram" className='hover:border-2 border-purple-300'/></a>
-            </li>
-          </ul>
+    <main>
+      <div className='bg-[#121214] bg-center bg-cover grid'>
+        <img src="./images/Desktop.svg" alt="gradiente do fundo" className="fixed rotate-90 ml-44 mt-56 md:rotate-0 md:m-0 md:absolute md:w-full md:object-contain"/>
+        <Ficha1 />
       </div>
     </main>
   )
