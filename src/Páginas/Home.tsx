70c3f7f621473e37d6a6fa91c424a8f4a55b6ef7
Luiz Domingues <luizarthur1234@gmail.com>
2022-05-22 23:16:19 -0300

    -

--- src/Páginas/Home.tsx
+++ src/Páginas/Home.tsx
@@ -6,9 +6,11 @@ export default function Home() {
     return(
         <main>
             <div className='bg-[#121214] bg-center bg-cover grid'>
-                <img src="../../images/Desktop.svg" alt="gradiente do fundo" className="fixed rotate-90 ml-44 mt-56 md:rotate-0 md:m-0 md:absolute md:w-full md:h-screen md:object-cover"/>
+                <img src="../../images/Desktop.svg" alt="gradiente do fundo" className="absolute m-0 w-full h-screen object-cover"/>
                 <Ficha1 />
-                <Footer />
+                <div className="hidden md:block">
+                    <Footer />
+                </div>
             </div>
         </main>
     )
